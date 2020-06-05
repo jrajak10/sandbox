1c2808f8ca44eb08db86fb646ac393e4f55ec125
Johann <38230021+jrajak10@users.noreply.github.com>
2020-06-05 14:58:55 +0100

    Made counties json file smaller, added choropleth map for recipients (#120)
    * made counties json file smaller, added choropleth map for recipients
    
    * used lodash to simplify function
    
    * simplified if function
    
    * improved the names of functions/variables, cleaned code
    
    Co-authored-by: Johann.Rajakarunanayake <jrajakarunanayake@gmail.com>

--- docs/geovation-community-map/community_map_functions.js
+++ docs/geovation-community-map/community_map_functions.js
@@ -74,6 +74,96 @@ function toggleInput(id, map) {
     });
 };
 
+function countUniqueCounties(data){
+    let object = {}
+
+    for(let i=0; i<data.length; i++){
+        if (object[data[i]["County"]] > 0){
+            object[data[i]["County"]]++
+        }
+        else{
+            object[data[i]["County"]] = 1
+        }
+    }
+    return object;
+}
+
+/**
+ * 
+ * @param {*Object[]} countyPolygons 
+ * @param {*Object} recipientsCount 
+ */
+function calculateCountyColors(countyPolygons, recipientsCount) {
+    let allCounties = _.map(_.uniqBy(countyPolygons, 'properties.NAME'));
+    let countyColors = [];
+    let color = "#FFF";
+
+    for (let county in allCounties) {
+        let countyName = allCounties[county].properties["NAME"];
+        let recipientsInCounty = recipientsCount[countyName];
+        if (!recipientsInCounty) {
+            color = "#FFF";
+        }
+        else {
+            color = recipientsInCounty <= 2 ? "#d3e8d3" : "#228b22";
+        }
+        countyColors.push(countyName, color);
+    }
+    //default value
+    countyColors.push("#000");
+    return countyColors;
+}
+
+function addCountiesOutline(map, countyPolygons){
+    map.addLayer({
+        'id': 'counties-outline',
+        'type': 'line',
+        'source': {
+            'type': 'geojson',
+            'data': {
+                "type": "FeatureCollection",
+                "features": countyPolygons
+            }
+        },
+        'paint': {
+            'line-color': '#000',
+            'line-width': 1
+        }
+    });
+}
+
+function addChoroplethLayer(map, id, expression, countyPolygons){
+    map.addLayer({
+        "id": id,
+        "type": "fill",
+        "source": {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": countyPolygons
+            }
+        },
+        "layout": {},
+        "paint": {
+            "fill-color": expression,
+            "fill-opacity": 0.8
+        }
+    });
+}
+
+//returns information about the county, and number of recipients at the bottom of the information box
+function addRecipientsInformation(map, recipientsCount) {
+    map.on('click', 'recipients', function (e) {
+        let recipientsTotal = recipientsCount[e.features[0].properties["NAME"]]
+        //make recipientsTotal return a value, else it will return 'undefined'
+        if (!recipientsTotal) {
+            recipientsTotal = 0;
+        }
+        document.getElementById('onclick-information').innerHTML = "County: " + e.features[0].properties["NAME"]
+            + "<br> Number of Recipients: " + recipientsTotal;
+    });
+}
+
 function addMapFeatures(map, popup) {
     map.dragRotate.disable(); // Disable map rotation using right click + drag.
     map.touchZoomRotate.disableRotation(); // Disable map rotation using touch rotation gesture.
@@ -87,14 +177,28 @@ function addMapFeatures(map, popup) {
         let partnerHubs = await fetchData('partner_hubs.json');
         let sponsors = await fetchData('sponsors.json');
         let stakeholders = await fetchData('stakeholders.json');
-       
+        //Fetches the polygons of all the UK counties. 
+        let countyPolygons = await fetchData('counties.json');
+        let recipients = await fetchData('geovation_recipients.json');
+        let recipientsCount = countUniqueCounties(recipients);
+        
+        // expression gives the colours for the map based on its value
+        let expression = ['match', ['get', 'NAME']];
+        let countyColors = (calculateCountyColors(countyPolygons, recipientsCount));
+        expression = expression.concat(countyColors)
+
+        addChoroplethLayer(map, 'recipients', expression, countyPolygons);
+        addCountiesOutline(map, countyPolygons);
+        addRecipientsInformation(map, recipientsCount);
+
         createMarkers(partnerHubs, map, 'partner-hub-markers', 'partner-hub-markers', 'partner_hubs_marker.png')
-        createMarkers(sponsors, map, 'sponsor-markers','sponsor-markers', 'sponsors_marker.png')
+        createMarkers(sponsors, map, 'sponsor-markers', 'sponsor-markers', 'sponsors_marker.png')
         createMarkers(stakeholders, map, 'stakeholder-markers', 'stakeholder-markers', 'stakeholder_marker.png')
 
         toggleInput('partner-hub-markers', map);
         toggleInput('sponsor-markers', map)
-        toggleInput('stakeholder-markers', map)     
+        toggleInput('stakeholder-markers', map);
+        toggleInput('recipients', map);
     });
 
     addPopup(map, 'partner-hub-markers', popup)
